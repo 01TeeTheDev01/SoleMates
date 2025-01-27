e5d343cfb1be0b6807ed0b0bb62f738627bda683
Teethedev@outlook.com <Teethedev@outlook.com>
2025-01-27 23:29:01 +0200

    Improved all pages and added more media

--- src/WA/SoleMatesWA/Controllers/HomeController.cs
+++ src/WA/SoleMatesWA/Controllers/HomeController.cs
@@ -18,6 +18,60 @@ public class HomeController : Controller
     [Route("about")]
     public IActionResult About() => View();
 
+    [Route("gallery")]
+    public IActionResult Gallery()
+    {
+        if (HttpContext.Request.Path.HasValue &&
+            !string.IsNullOrEmpty(HttpContext.Request.Path.Value) &&
+            HttpContext.Request.Path.Value.Equals("/gallery"))
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images");
+
+            var images = Directory.GetFiles(path)
+                .Select(file => Path.GetFileName(file))
+                .ToArray();
+
+            ViewData["Images"] = images;
+
+            HttpContext.Response.Headers.Append("Path", HttpContext.Request.Path.Value);
+            HttpContext.Response.StatusCode = StatusCodes.Status200OK;
+            return View();
+        }
+        else
+        {
+            HttpContext.Response.Headers.Append("Path", HttpContext.Request.Path.Value);
+            HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            return BadRequest();
+        }
+    }
+
+    [Route("events")]
+    public IActionResult Events()
+    {
+        if (HttpContext.Request.Path.HasValue &&
+                !string.IsNullOrEmpty(HttpContext.Request.Path.Value) &&
+                HttpContext.Request.Path.Value.Equals("/events"))
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "videos");
+
+            var videos = Directory.GetFiles(path)
+                .Select(file => Path.GetFileName(file))
+                .ToArray();
+
+            ViewData["Videos"] = videos;
+
+            HttpContext.Response.Headers.Append("Path", HttpContext.Request.Path.Value);
+            HttpContext.Response.StatusCode = StatusCodes.Status200OK;
+            return View();
+        }
+        else
+        {
+            HttpContext.Response.Headers.Append("Path", HttpContext.Request.Path.Value);
+            HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            return BadRequest();
+        }
+    }
+
     [Route("/")]
     public IActionResult Index() => View();
 
