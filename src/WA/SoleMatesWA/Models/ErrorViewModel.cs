0a0cf96552754d6b204edce83c038cc9f633d0c9
Teethedev@outlook.com <Teethedev@outlook.com>
2025-01-27 15:18:27 +0200

    Initial commit with dummy code

---
+++ src/WA/SoleMatesWA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SoleMatesWA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
