7437fee9660e8cd6dbac7d21331194696109d30f
01TeeTheDev01 <01TeeTheDev01@users.noreply.github.com>
2025-01-27 21:33:49 +0000

    deploy: 1e5e8fe56dcfcb5377de70b62412ad4d3fbef150

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,20 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const previewModal = document.getElementById('imagePreviewModal');
+    const previewImage = document.getElementById('previewImage');
+    const body = document.body;
 
-// Write your JavaScript code.
+    // Attach event listener for each preview image
+    document.querySelectorAll('.preview-image').forEach(img => {
+        img.addEventListener('click', function () {
+            const imageSrc = this.getAttribute('data-src');
+            previewImage.src = imageSrc;
+            //body.style.opacity = "0.5"; 
+            previewImage.style.opacity = "1"; // Ensure the image has full opacity
+        });
+    });
+
+    previewModal.addEventListener('hidden.bs.modal', () => {
+        body.style.opacity = "1"; // Restore normal opacity
+        previewImage.src = ""; // Clear the image source when modal closes
+    });
+});
