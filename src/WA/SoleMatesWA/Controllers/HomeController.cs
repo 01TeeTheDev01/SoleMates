0cc4dadfa4cced42e6664b17824f108695929720
Teethedev@outlook.com <Teethedev@outlook.com>
2025-01-27 16:52:12 +0200

    Updated colors and added About page

--- src/WA/SoleMatesWA/Controllers/HomeController.cs
+++ src/WA/SoleMatesWA/Controllers/HomeController.cs
@@ -1,32 +1,32 @@
 using System.Diagnostics;
+
 using Microsoft.AspNetCore.Mvc;
+
 using SoleMatesWA.Models;
 
-namespace SoleMatesWA.Controllers
+namespace SoleMatesWA.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
+        _logger = logger;
+    }
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+    [Route("about")]
+    public IActionResult About() => View();
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+    [Route("/")]
+    public IActionResult Index() => View();
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+    [Route("privacy")]
+    public IActionResult Privacy() => View();
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
